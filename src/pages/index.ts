4c5149a06bacbcc4c6c97f7dec95ee5d74a7b8b4
kenizaya <harveylstark@gmail.com>
2022-10-28 14:32:38 +0530

    Setup Routes

---
+++ src/pages/index.ts
@@ -0,0 +1,10 @@
+import EditProfile from './EditProfile'
+import Explore from './Explore'
+import Feed from './Feed'
+import Login from './Login'
+import NotFound from './NotFound'
+import Post from './Post'
+import Profile from './Profile'
+import Signup from './Signup'
+
+export { EditProfile, Explore, Feed, Login, NotFound, Post, Profile, Signup }
