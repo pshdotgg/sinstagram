71251aedfa5f31abe89062c02433eae309976b89
kenizaya <harveylstark@gmail.com>
2022-10-31 14:31:36 +0530

    Add Follow Button

--- src/components/shared/FollowButton.tsx
+++ src/components/shared/FollowButton.tsx
@@ -1,7 +1,36 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-const FollowButton = () => {
-  return <div>FollowButton</div>
+const FollowButton = ({ side }) => {
+  const [isFollowing, setIsFollowing] = useState(false)
+  const followButton = (
+    <button
+      type='button'
+      className={
+        side
+          ? 'btn-link btn-sm no-underline text-primary'
+          : 'text-white rounded py-1  mt-2 bg-primary border-transparent hover:bg-primary hover:border-transparent'
+      }
+      onClick={() => setIsFollowing(true)}
+    >
+      Follow
+    </button>
+  )
+
+  const followingButton = (
+    <button
+      type='button'
+      className={
+        side
+          ? 'btn btn-outline btn-xs text-gray-900 font-normal px-1 rounded normal-case bg-white hover:bg-base-100 hover:text-gray-900'
+          : 'text-white rounded py-1  mt-2 bg-primary border-transparent hover:bg-primary hover:border-transparent'
+      }
+      onClick={() => setIsFollowing(false)}
+    >
+      Following
+    </button>
+  )
+
+  return isFollowing ? followingButton : followButton
 }
 
 export default FollowButton
